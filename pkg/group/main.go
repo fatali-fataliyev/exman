07dbf8ea506d3a25008801b48588064bcea50305
fatali-fataliyev <fatali.fataliiyev@gmail.com>
2025-02-09 22:05:07 +0400

    group package created and CreateGroup function exported

--- pkg/group/main.go
+++ pkg/group/main.go
@@ -1,5 +1,24 @@
 package group
 
-func createGroup(groupName string) {
+import (
+	"fmt"
+)
+
+type Group struct {
+	name         string
+	members      int //after retype to User type.
+	creationDate string
+}
+
+func CreateGroup(groupName string) (Group, error) {
+	//creates group name
+	//add user functionality.
+	//add current time to group.
+
+	if groupName == "" {
+		return Group{}, fmt.Errorf("group name is empty, write a normal group name please.")
+	}
+
+	return Group{}, nil
 
 }
