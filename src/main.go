07162210e37d398f7409e9e2af8f16c859360941
fatali-fataliyev <fatali.fataliiyev@gmail.com>
2025-02-09 22:03:13 +0400

    group package created and CreateGroup function exported

--- src/main.go
+++ src/main.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/fatali-fataliyev/exman/pkg/group"
+)
 
 func main() {
 	fmt.Println("a simple expenses manager between groups.")
-
+	grp1, err := group.CreateGroup("Fatali")
+	if err != nil {
+		fmt.Println("failed to give name to group: ", err)
+	}
+	fmt.Println(grp1)
 }
